cb0c9c8a70504e84ec83cc76ce8def713496d301
Raji <rajisunusi@gmail.com>
2025-03-09 15:44:57 +0100

    Refactor imports in filters.py to use wildcard import for models

--- inventory/filters.py
+++ inventory/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Item, Record, ReStock
+from .models import *
 from django import forms
 from django.utils import timezone
 from datetime import datetime, time
