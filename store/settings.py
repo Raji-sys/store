3f488a90d63006433b3e18fcc4cbc4247900d78b
Raji <rajisunusi@gmail.com>
2025-03-09 16:26:12 +0100

    Refactor settings.py to comment out Redis cache settings and switch database to SQLite for development

--- store/settings.py
+++ store/settings.py
@@ -5,9 +5,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-o9p7!!(y$7d1a5j@2d)u^cw$gq&bc5z_d9^es9ob_q8$&o8-x#'
 
@@ -34,7 +31,7 @@ INSTALLED_APPS = [
     'django_fastdev',
     'import_export',
     'simple_history',
-    'redisboard',
+    # 'redisboard',
 ]
 
 MIDDLEWARE = [
@@ -49,18 +46,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379",
+#     }
+# }
 
 
-# # Cache settings
-CACHE_MIDDLEWARE_SECONDS = 60 * 60
-CACHE_MIDDLEWARE_KEY_PREFIX = "store"
-CACHE_MIDDLEWARE_ALIAS = "default"
+# # # Cache settings
+# CACHE_MIDDLEWARE_SECONDS = 60 * 60
+# CACHE_MIDDLEWARE_KEY_PREFIX = "store"
+# CACHE_MIDDLEWARE_ALIAS = "default"
 
 ROOT_URLCONF = 'store.urls'
 
@@ -89,20 +86,25 @@ NPM_BIN_PATH = "/usr/bin/npm"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE':'django.db.backends.postgresql',
+#         # 'NAME':'store_nohd',
+#         'NAME':'nohd_store',
+#     	# 'USER':'sadmin',
+# 	    # 'PASSWORD':'s2024db',
+#     	'USER':'postgres',
+# 	    'PASSWORD':'8080mali',
+# 	    'HOST':'localhost',
+# 	    'PORT':'5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql',
-        # 'NAME':'store_nohd',
-        'NAME':'nohd_store',
-    	# 'USER':'sadmin',
-	    # 'PASSWORD':'s2024db',
-    	'USER':'postgres',
-	    'PASSWORD':'8080mali',
-	    'HOST':'localhost',
-	    'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -160,8 +162,6 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Log out when browser closes
 SESSION_SAVE_EVERY_REQUEST = True  # Reset session timeout on activity
 SESSION_COOKIE_SECURE = False  # Set to True in production with HTTPS
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
